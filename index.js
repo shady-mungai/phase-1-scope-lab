23a7812c904a61e0768a8300929e0531aa7000ca
shady-mungai <shadrackmungai10@gmail.com>
2022-12-05 11:47:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+ function upperCaseCustomerName ()
+{
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+setBestCustomer = () => {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+overwriteBestCustomer = () =>
+{
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavCustomer = "Kelvin"
+changeLeastFavoriteCustomer = () =>
+{
+    leastFavCustomer = "Mike";
+   return leastFavCustomer;
+}
